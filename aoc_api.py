b24d005950d7d08718870b508fc42e7233d54899
Marvin Stallmann <marvin.stallmann@edu.fhdw.de>
2024-12-05 18:56:34 +0100

    Enable submition of solution via api

--- aoc_api.py
+++ aoc_api.py
@@ -1,8 +1,15 @@
 import requests
 import local
+import re
+
+AOC_URL = 'https://adventofcode.com/2024/day/{}'
+HEADERS = {
+    'cookie': 'session={0}'.format(local.session)
+}
 
 
-def fetch_input(day: int, test: bool = False):
+# Fetch Puzzle Input
+def get_puzzle_input(day: int, test: bool = False):
     text = read_test_data(day) if test else fetch_data(day)
     input_values = text.splitlines()
     return input_values
@@ -12,10 +19,16 @@ def read_test_data(day: int) -> str:
     return open(f'./test_data/day{day}.txt').read()
 
 
-
 def fetch_data(day: int) -> str:
-    url = f'https://adventofcode.com/2024/day/{day}/input'
-    headers = {
-        'cookie': 'session={0}'.format(local.session)
-    }
-    return requests.get(url, headers=headers).text
+    url = AOC_URL.format(day) + '/input'
+    return requests.get(url, headers=HEADERS).text
+
+
+# Submit Puzzle Solution
+def submit_solution(day: int, part: int, value: str) -> str:
+
+    url = AOC_URL.format(day) + '/answer'
+    response = requests.post(url, data={'level': part, 'answer': str(value)}, headers=HEADERS).text
+    response = re.search(r'<main>([\S\s]*)</main>', response).group(0)
+    response = re.sub(r'<(.*?)>', '', response)
+    return response
--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import time
 import aoc_api
 from puzzles import historyan_hysteria, red_nosed_reports, mull_it_over, ceres_search, print_queue
 
-selected = 1
+selected_day = 1
+solution = None
+
 in_menu = True
 puzzles = {
     'Historian Hysteria': historyan_hysteria.solve,
@@ -31,19 +33,19 @@ def show_menu():
     for i in range(1, len(puzzles) + 1):
         print('{2} Day {0}: {1} {3}'.format(i,
                                             list(puzzles.keys())[i - 1],
-                                            ">" if selected == i else " ",
-                                            "<" if selected == i else " "))
+                                            ">" if selected_day == i else " ",
+                                            "<" if selected_day == i else " "))
     print('\n[˄]Up [˅]Down [⏎]Run [T]Test [esc]Exit')
 
 
 def navigate(direction):
     if in_menu:
-        global selected
-        selected = selected + direction
-        if selected < 1:
-            selected = len(puzzles)
-        elif selected > len(puzzles):
-            selected = 1
+        global selected_day
+        selected_day = selected_day + direction
+        if selected_day < 1:
+            selected_day = len(puzzles)
+        elif selected_day > len(puzzles):
+            selected_day = 1
         show_menu()
 
 
@@ -56,25 +58,39 @@ def run(test: bool = False):
     mode = '[TEST] ' if test else ''
 
     clear()
-    print(f'{mode}Puzzle result day {selected}:')
+    print(f'{mode}Puzzle result day {selected_day}:')
     print('--------------------------------------')
 
     try:
-        puzzle_input = aoc_api.get_puzzle_input(selected, test)
-        puzzle_solver = list(puzzles.values())[selected-1]
+        global solution
+        puzzle_input = aoc_api.get_puzzle_input(selected_day, test)
+        puzzle_solver = list(puzzles.values())[selected_day - 1]
 
         start = time.time()
-        (solution1, solution2) = puzzle_solver(puzzle_input)
+        solution = puzzle_solver(puzzle_input)
         stop = time.time()
 
-        print("Solution 1:", solution1)
-        print("Solution 2:", solution2)
+        print("Solution 1:", solution[0])
+        print("Solution 2:", solution[1])
 
         print('\nExecution time: {}s'.format(round(stop-start, 3)))
     except FileNotFoundError:
-        print(f'No test data found for day {selected}!')
+        print(f'No test data found for day {selected_day}!')
 
-    print('\n[<--]Show Menu [esc]Exit')
+    if test:
+        print('\n[<--]Menu [esc]Exit')
+    else:
+        print('\n[<--]Menu [cmd+F1]Submit Part 1 [cmd+F2]Submit Part 2 [esc]Exit')
+
+
+def submit(part: int):
+    if (not solution or
+            in_menu):
+        return
+
+    clear()
+    print(aoc_api.submit_solution(selected_day, part, solution[part-1]))
+    print('\n[<--]Menu [esc]Exit')
 
 
 show_menu()
@@ -84,9 +100,11 @@ with keyboard.GlobalHotKeys({
     '<left>': lambda: navigate(-1),
     '<down>': lambda: navigate(1),
     '<right>': lambda: navigate(1),
+    '<cmd>+<enter>': lambda: run(test=True),
     '<enter>': run,
     '<backspace>': show_menu,
-    't': lambda: run(test=True),
+    '<cmd>+<f1>': lambda: submit(1),
+    '<cmd>+<f2>': lambda: submit(2),
     '<esc>': lambda: keyboard_listener.stop(),
 }) as keyboard_listener:
     keyboard_listener.join()
